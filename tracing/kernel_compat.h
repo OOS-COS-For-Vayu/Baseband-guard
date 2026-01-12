95f586616a546dcfa268893cebb0fb5c6a898166
momenabdulrazek <momenabdulrazek@users.noreply.github.com>
2026-01-13 00:17:17 +0200

    let "selinux_state.initialized" used for kernel 4.14 and newer
    because nowadays most of 4.14.x kernels has a lot of selinux backports from newer kernel releases so it will no longer use obsolte ss_initialized
    
    * Reference: https://github.com/LineageOS/android_kernel_google_msm-4.14/commit/b86e8759d93cce8d89d227760e464fcc7f3f1370
    
    Signed-off-by: momenabdulrazek <momen.abdelrazek33@gmail.com>

--- tracing/kernel_compat.h
+++ tracing/kernel_compat.h
@@ -14,7 +14,7 @@ static __maybe_unused inline bool selinux_initialized_compat(void)
     return selinux_initialized();
 #elif LINUX_VERSION_CODE >= KERNEL_VERSION(5,6,0)
     return selinux_initialized(&selinux_state);
-#elif LINUX_VERSION_CODE >= KERNEL_VERSION(4,17,0)
+#elif LINUX_VERSION_CODE >= KERNEL_VERSION(4,14,0)
     return selinux_state.initialized;
 #else
     return ss_initialized;
